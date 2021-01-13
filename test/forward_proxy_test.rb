61fe6bdc5f811d9544142b4141dc2afb2de80f23
James Moriarty <james.moriarty@innablr.com.au>
2021-01-14 08:00:40 +1100

    refinement: cleanup testing and gemspec.

--- test/forward_proxy_test.rb
+++ test/forward_proxy_test.rb
@@ -1,23 +1,12 @@
 require "test_helper"
 
-ENV['https_proxy'] = "http://127.0.0.1:3000"
-
 class ForwardProxyTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::ForwardProxy::VERSION
-  end
-
   def proxy(uri)
-    hostname = "127.0.0.1"
-    port = 3000
-
-    server = ForwardProxy::Server.new(bind_address: hostname, bind_port: port)
-
+    server = ForwardProxy::Server.new(bind_address: "127.0.0.1", bind_port: 3000)
     Thread.new { server.start }
-
     sleep 1
 
-    Net::HTTP.start(uri.hostname, uri.port, hostname, port, use_ssl: uri.scheme == 'https') do |http|
+    Net::HTTP.start(uri.hostname, uri.port, server.bind_address, server.bind_port, use_ssl: uri.scheme == 'https') do |http|
       yield http.request Net::HTTP::Get.new(uri)
     end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "forward_proxy"
 
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 require "minitest/autorun"
+
