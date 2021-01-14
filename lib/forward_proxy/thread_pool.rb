4bffd92fcc34889f45d3b9a87d6a3a571c9aa111
James Moriarty <jamespaulmoriarty@gmail.com>
2021-01-14 20:31:15 +1100

    refinement: wrap thread pool in module.

--- lib/forward_proxy/thread_pool.rb
+++ lib/forward_proxy/thread_pool.rb
@@ -1,34 +1,36 @@
-class ThreadPool
-  attr_reader :queue, :threads, :size
+module ForwardProxy
+  class ThreadPool
+    attr_reader :queue, :threads, :size
 
-  def initialize(size)
-    @size    = size
-    @queue   = Queue.new
-    @threads = []
-  end
+    def initialize(size)
+      @size    = size
+      @queue   = Queue.new
+      @threads = []
+    end
 
-  def start
-    size.times do
-      threads << Thread.new do
-        catch(:exit) do
-          loop do
-            job, args = queue.pop
-            job.call(*args)
+    def start
+      size.times do
+        threads << Thread.new do
+          catch(:exit) do
+            loop do
+              job, args = queue.pop
+              job.call(*args)
+            end
           end
         end
       end
     end
-  end
 
-  def schedule(*args, &block)
-    queue.push([block, args])
-  end
-
-  def shutdown
-    threads.each do
-      schedule { throw :exit }
+    def schedule(*args, &block)
+      queue.push([block, args])
     end
 
-    threads.each(&:join)
+    def shutdown
+      threads.each do
+        schedule { throw :exit }
+      end
+
+      threads.each(&:join)
+    end
   end
 end
