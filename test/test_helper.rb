7ee399148878cba586d1ff88e5d3d305ea0f98d1
James Moriarty <james.moriarty@innablr.com.au>
2021-01-14 20:40:37 +1100

    refinement: move test helper out of test file.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,26 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require "minitest/autorun"
+
+def request(uri, req = Net::HTTP::Get.new(uri))
+    proxy = ForwardProxy::Server.new(
+      bind_address: "127.0.0.1",
+      bind_port: 3000
+    )
+
+    Thread.new { proxy.start }
+
+    sleep 1
+
+    Net::HTTP.start(
+      uri.hostname,
+      uri.port,
+      proxy.bind_address,
+      proxy.bind_port,
+      use_ssl: uri.scheme == 'https'
+    ) do |http|
+      yield http.request req
+    end
+
+    proxy.shutdown
+  end
