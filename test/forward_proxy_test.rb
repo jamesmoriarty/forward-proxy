c24ca795e4d9720f1a17c4399b75a9b2178b34d0
James Moriarty <james.moriarty@innablr.com.au>
2021-01-17 15:20:48 +1100

    refinement: cleanup test helper signature.

--- test/forward_proxy_test.rb
+++ test/forward_proxy_test.rb
@@ -2,7 +2,11 @@ require "test_helper"
 
 class ForwardProxyTest < Minitest::Test
   def test_handle_get
-    request(URI('http://google.com')) do |resp|
+    uri = URI('http://google.com')
+
+    with_proxy(uri) do |http|
+      resp = http.request Net::HTTP::Get.new(uri)
+
       assert_equal "301", resp.code
       assert_equal "HTTP/1.1 ForwardProxy", resp['via']
       assert_equal "http://www.google.com/", resp['location']
@@ -21,7 +25,9 @@ class ForwardProxyTest < Minitest::Test
   def test_handle_post
     uri = URI('http://google.com')
 
-    request(uri, Net::HTTP::Post.new(uri)) do |resp|
+    with_proxy(uri) do |http|
+      resp = http.request Net::HTTP::Post.new(uri)
+
       assert_equal "405", resp.code
       assert_equal "HTTP/1.1 ForwardProxy", resp['via']
       refute_nil resp.body
@@ -31,14 +37,20 @@ class ForwardProxyTest < Minitest::Test
   def test_handle_with_unsupported_method
     uri = URI('http://google.com')
 
-    request(uri, Net::HTTP::Trace.new(uri)) do |resp|
+    with_proxy(uri) do |http|
+      resp = http.request Net::HTTP::Trace.new(uri)
+
       assert_equal "502", resp.code
       assert_equal "HTTP/1.1 ForwardProxy", resp['via']
     end
   end
 
   def test_handle_tunnel
-    request(URI('https://google.com')) do |resp|
+    uri = URI('https://google.com')
+
+    with_proxy(uri) do |http|
+      resp = http.request Net::HTTP::Get.new(uri)
+
       assert_equal "301", resp.code
       assert_equal "https://www.google.com/", resp['location']
       body = <<~eos
