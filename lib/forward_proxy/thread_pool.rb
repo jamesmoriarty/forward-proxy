d1634da994eaa73d180b6573d95a591f57e2f233
James Moriarty <jamespaulmoriarty@gmail.com>
2021-07-04 21:27:40 +1000

    refinement: remove thread shutdown code.

--- lib/forward_proxy/thread_pool.rb
+++ lib/forward_proxy/thread_pool.rb
@@ -11,11 +11,9 @@ module ForwardProxy
     def start
       size.times do
         threads << Thread.new do
-          catch(:exit) do
-            loop do
-              job, args = queue.pop
-              job.call(*args)
-            end
+          loop do
+            job, args = queue.pop
+            job.call(*args)
           end
         end
       end
@@ -23,14 +21,6 @@ module ForwardProxy
 
     def schedule(*args, &block)
       queue.push([block, args])
-    end
-
-    def shutdown
-      threads.each do
-        schedule { throw :exit }
-      end
-
-      threads.each(&:join)
     end
   end
 end
